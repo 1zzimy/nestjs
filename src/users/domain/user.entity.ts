5f1fd478b6366a550646d2ab2477e63c6a5ecf7e
이지민 <1zzimy@naver.com>
2025-10-27 00:25:12 +0900

    refactor: User 엔티티 개선 및 디렉토리 이동

--- src/users/domain/user.entity.ts
+++ src/users/domain/user.entity.ts
@@ -1,17 +1,18 @@
+import { BaseTimeEntity } from "src/common/entities/base-time.entity";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
-export class User {
+export class User extends BaseTimeEntity{
     @PrimaryGeneratedColumn()
     id: number;
 
     @Column()
     name: string;
 
-    @Column()
+    @Column({ unique: true })
     email: string;
 
-    @Column()
+    @Column({ select: false })
     pwd: string;
 
     @Column({default: true})
