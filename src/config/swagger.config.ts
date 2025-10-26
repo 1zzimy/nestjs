cd03e4edd8b31203f2375f2f16b92b5351bb13cb
이지민 <1zzimy@naver.com>
2025-10-27 00:22:11 +0900

    feat: Swagger 설정에 로컬 서버 URL 추가

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -4,5 +4,6 @@ export const swaggerConfig = new DocumentBuilder()
   .setTitle('NestJS API Docs')
   .setDescription('NestJS 공부용')
   .setVersion('v1.0')
+  .addServer('http://localhost:3000')
   .addTag('User', '회원 관련 API')
   .build();
