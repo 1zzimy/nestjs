b2b7640b28254e8361dc95911a8abb69e77d2018
이지민 <1zzimy@naver.com>
2025-10-27 02:55:12 +0900

    feat: JWT 인증 모듈 및 전역 가드 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './config/typeorm.config';
 import { ApiResultInterceptor } from './common/interceptors/api-result.interceptor';
 import { ApiExceptionFilter } from './common/filters/api-exception.filter';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
+import { RedisModule } from './common/redis/redis.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
-    UsersModule
+    RedisModule,
+    UsersModule,
+    AuthModule,
   ],
   providers: [ApiResultInterceptor,{
       provide: APP_FILTER,
       useClass: ApiExceptionFilter,
-    },],
+    },{
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    }],
 })
 export class AppModule {}
