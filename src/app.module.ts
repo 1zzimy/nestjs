fd5409b12a6fcb1a72e21a3504fdf95c50fd749f
이지민 <1zzimy@naver.com>
2025-10-27 00:19:54 +0900

    feat: ApiExceptionFilter를 전역 예외 필터로 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,18 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './config/typeorm.config';
+import { ApiResultInterceptor } from './common/interceptors/api-result.interceptor';
+import { ApiExceptionFilter } from './common/filters/api-exception.filter';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
     UsersModule
   ],
+  providers: [ApiResultInterceptor,{
+      provide: APP_FILTER,
+      useClass: ApiExceptionFilter,
+    },],
 })
 export class AppModule {}
